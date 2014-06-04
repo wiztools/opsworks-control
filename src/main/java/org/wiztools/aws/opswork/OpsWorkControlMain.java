626b1d5a7ec0bccea02f83bc85a5ed61d0131d94
Subhash Chandran <subwiz@gmail.com>
2014-06-04 23:50:56 +0530

    unused import removed

--- src/main/java/org/wiztools/aws/opswork/OpsWorkControlMain.java
+++ src/main/java/org/wiztools/aws/opswork/OpsWorkControlMain.java
@@ -8,7 +8,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
